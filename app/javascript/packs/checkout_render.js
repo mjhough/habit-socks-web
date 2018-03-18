1e7452fe9c9f23cb1ac22272e85cb07a8f835a22
Matt Hough <matt@hough.tv>
2018-03-18 22:12:23 +1000

    Remove obsolete checkout_render

--- app/javascript/packs/checkout_render.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Checkout from '../components/Checkout';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const node = document.getElementById('checkout_render')
-
-  ReactDOM.render(<Checkout name='Habit Socks' description='A sock charity' amount={16.50} />, node);
-})
