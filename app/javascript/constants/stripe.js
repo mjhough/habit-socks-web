617ba0b63ae0d1b649ec1ac7d0126d3daee9dc01
Matt Hough <matt@hough.tv>
2018-03-18 11:55:21 +1000

    Expire exposed tokens

--- app/javascript/constants/stripe.js
+++ app/javascript/constants/stripe.js
@@ -1,5 +1,5 @@
 const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_live_OEo4Fj3Pb21IVWmWSvYI8zUG'
-  : 'pk_test_mq7yUzQWFdLTp3tV7UoRKxXW';
+  ? 'expired'
+  : 'expired';
 
 export default STRIPE_PUBLISHABLE;
--- backend/constants/stripe.js
+++ backend/constants/stripe.js
@@ -1,8 +1,8 @@
 const configureStripe = require('stripe');
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-  ? 'sk_live_N14twY2mXN8r04W80HuPX56R' 
-  : 'sk_test_2EKwBcSjSvCcT73YBiZtibSe';
+  ? 'expired' 
+  : 'expired';
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
