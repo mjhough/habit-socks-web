068c102403cbceb17e48a79b6e22c9a58d2bf8ee
Matt Hough <matt@hough.tv>
2018-03-20 09:45:37 +1000

    Move backend into separate app

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: bundle exec puma -C config/puma.rb
-express: cd ./backend/ && PORT=8080 node index.js
+web: node index.js
--- backend/constants/stripe.js
+++
@@ -1,9 +0,0 @@
-const configureStripe = require('stripe');
-
-const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-  ? process.env.STRIPE_SECRET
-  : process.env.STRIPE_TEST_SECRET;
-
-const stripe = configureStripe(STRIPE_SECRET_KEY);
-
-module.exports = stripe;
