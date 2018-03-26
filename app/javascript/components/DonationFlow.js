78affd2083fad283fb99462b6c6623faac8c7551
Matt Hough <matt@hough.tv>
2018-03-26 22:51:06 +1000

    Restrict donation for now

--- app/javascript/components/DonationFlow.js
+++ app/javascript/components/DonationFlow.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import postal from 'postal';
 import Checkout from './Checkout';
+import DonationSuccess from './DonationSuccess';
 
 export default class DonationFlow extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { amount: 5 }
+    this.state = {
+      amount: null,
+      success: false
+    }
+  }
+
+  notice() {
+    alert('Unfortunately, while Habit Socks waits for the result of our registered charity application, we cannot accept donations');
+  }
+
+  componentDidMount() {
+    postal.subscribe({
+      channel: 'checkout',
+      topic: 'checkout.success',
+      callback: this.checkoutSuccess.bind(this)
+    });
+  }
+
+  checkoutSuccess(data, env) {
+    const checkoutData = JSON.parse(data.data).success;
+    if (checkoutData.status === 'paid') this.setState({
+      success: true,
+      checkoutData: checkoutData
+    });
   }
 
   handleChange(ev) {
@@ -12,6 +37,12 @@ export default class DonationFlow extends React.Component {
   }
   
   render() {
+    if (this.state.success) {
+      const customerName = this.state.checkoutData.shipping.name.split(' ')[0];
+      return (
+        <DonationSuccess customerName={customerName} />
+      )
+    }
     return (
       <section className='switchable'>
         <div className='container'>
@@ -27,7 +58,7 @@ export default class DonationFlow extends React.Component {
                 <form>
                   <input className='validate-required dollar-input' type='number' name='amount' placeholder='Donation Amount' onChange={this.handleChange.bind(this)} />
                   <textarea className='top-margin-10' name='comment' placeholder='Write a small comment here if you would like to' />
-                  <Checkout name='Donation' description='Donate to Habit Socks' amount={this.state.amount} label='Donate to Habit Socks' buttonClassName='btn btn--primary type--uppercase top-margin-10' />
+                  <a onClick={this.notice} className='btn btn--primary type--uppercase top-margin-10'><span className='btn__text'>Donate to Habit Socks</span></a>
                 </form>
               </div>
             </div>
@@ -37,3 +68,6 @@ export default class DonationFlow extends React.Component {
     )
   }
 }
+
+
+  /* <Checkout name='Donation' description='Donate to Habit Socks' amount={this.state.amount} label='Donate to Habit Socks' buttonClassName='btn btn--primary type--uppercase top-margin-10' /> */
