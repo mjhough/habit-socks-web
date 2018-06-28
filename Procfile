17d18438c2d5b5bf0dc677b13e1ad510a5fdfb27
Matt Hough <matt@hough.tv>
2018-06-28 14:43:15 +1000

    add set and map polyfill's

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bundle exec puma -C config/puma.rb
+ngrok: ngrok http -subdomain=mhough 5000
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 // Support component names relative to this directory:
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+
 var componentRequireContext = require.context("components", true)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
